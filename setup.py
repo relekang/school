9fa380e6f24c1ae5f959f0e7a31920023bc3ef7d
Rolf <relekang@gmail.com>
2011-11-23 13:24:23 +0100

    init

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "",
+    version = "1.0",
+    url = 'http://',
+    license = 'Proprietary',
+    description = "",
+    author = 'Rolf Erik Lekang',
+    requires = [],
+    install_requires = ['setuptools'],
+)
+
+
