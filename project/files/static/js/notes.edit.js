c66b6a113f499705fcac6e383e9bafad3d8773aa
Rolf <relekang@gmail.com>
2011-11-27 16:16:19 +0100

    new save script

--- project/files/static/js/notes.edit.js
+++ project/files/static/js/notes.edit.js
@@ -1,12 +1,9 @@
 $(document).ready(function() {
-    $("#id_content").keydown(function(ev) {
-        key = ev.keyCode
-        if (!key) key = ev.charCode
+    $(window).keypress(function(event) {
+        if (!(event.which == 115 && event.ctrlKey) && !(event.which == 19)) return true;
+        $("form").submit();
+        event.preventDefault();
+        return false;
 
-        if (ev.type == 'keypress' && 115 == key && ev.ctrlKey) return false
-        if (83 == key && ev.ctrlKey) {
-            if (ev.type != 'keypress') $("form").submit()
-            return false
-        }
     });
 });
