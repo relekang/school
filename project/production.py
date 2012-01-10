1a5351b3f09100639b8bdcafd44043d0aa2d5e00
Rolf <relekang@gmail.com>
2012-01-10 13:43:57 +0100

    Update project/production.py

--- project/production.py
+++ project/production.py
@@ -3,14 +3,15 @@ from project.settings import *
 
 #CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
 
-with open('/django-sites/db-pw/0', 'rb') as f:
+with open('/django-sites/db-pw/django', 'rb') as f:
     db_password = f.readline()
 db_password = db_password.replace("\n","").strip()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'school',
-        'USER': 'rolf',
+        'USER': 'django',
         'PASSWORD': db_password,
         'HOST': '127.0.0.1',
         'PORT': ''
